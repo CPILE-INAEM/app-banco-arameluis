b738335e2d6b321ee3297b89b3f8771df57cbfcc
luis40 <luissg21@gmail.com>
2023-04-14 14:02:32 +0200

    Implementados las funciones de movimientos y elimnar cuenta

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
-
 // Data
 const account1 = {
   owner: 'Juan Sánchez',
@@ -35,10 +34,38 @@ const account4 = {
 
 const accounts = [account1, account2, account3, account4]
 
+// Metodo para formatear la fecha en el formato inicial que propone el ejercicio
+const formatDate = (fecha) => {
+  const year = fecha.getFullYear()
+  const month =
+    fecha.getMonth().toString.length < 2
+      ? `0${fecha.getMonth()}`
+      : fecha.getMonth()
+  const day =
+    fecha.getDay().toString.length < 2 ? `0${fecha.getDay()}` : fecha.getDay()
+  return `${year}-${month}-${day}`
+}
+
+// Metodo para crear fechas aleatorias
+
+const fakeDate = () => {
+  const yearRand = Math.floor(Math.random() * 3) + 2020
+  const year = yearRand.toString()
+  const monthRand = Math.floor(Math.random() * 12).toString()
+  const month = monthRand.length < 2 ? `0${monthRand}` : monthRand
+  const dayRand = Math.floor(Math.random() * 28).toString()
+  const day = dayRand.length < 2 ? `0${dayRand}` : dayRand
+  return `${year}-${month}-${day}`
+}
+
+// Metodo para generar cantidades aleatorias
+
+const fakeAmount = () => Math.floor(Math.random() * 200000 - 50000) / 100
+
 accounts.forEach((account) => {
-  account.movements = account.movements.map(
-    (a) => (a = { date: '2021-02-01', value: a })
-  )
+  account.movements = account.movements
+    .map((a) => (a = { date: fakeDate(), value: fakeAmount() }))
+    .sort((a, b) => a.date - b.date)
 })
 
 // Elements
@@ -54,6 +81,7 @@ const containerApp = document.querySelector('.app')
 const containerMovements = document.querySelector('.movements')
 
 const btnLogin = document.querySelector('.login__btn')
+const btnLogout = document.querySelector('.logout__btn')
 const btnTransfer = document.querySelector('.form__btn--transfer')
 const btnLoan = document.querySelector('.form__btn--loan')
 const btnClose = document.querySelector('.form__btn--close')
@@ -67,7 +95,10 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount')
 const inputCloseUsername = document.querySelector('.form__input--user')
 const inputClosePin = document.querySelector('.form__input--pin')
 
+let logedAccount = null
+
 //Init data
+
 const createUsernames = () => {
   accounts.forEach((account) => {
     account.username = account.owner
@@ -85,6 +116,7 @@ btnLogin.addEventListener('click', (e) => {
   const currentAccount = accounts.find(
     (account) => account.username === username
   )
+  logedAccount = currentAccount
 
   //Si la cuenta no existe porque no hay un usuario con ese nombre,
   // la variable currentAccount es nula y al acceder al pin de un objeto nulo da error
@@ -97,9 +129,23 @@ btnLogin.addEventListener('click', (e) => {
     containerApp.style.opacity = 100
     inputLoginUsername.value = inputLoginPin.value = ''
     inputLoginPin.blur()
+    btnLogin.style.display = 'none'
+    btnLogout.style.display = 'initial'
     updateUI(currentAccount)
   }
 })
+
+// Implementamos un boton de logout
+
+btnLogout.addEventListener('click', (e) => {
+  e.preventDefault()
+  btnLogin.style.display = 'initial'
+  btnLogout.style.display = 'none'
+  labelWelcome.textContent = 'Log in to get started'
+  containerApp.style.opacity = 0
+  logedAccount = null
+})
+
 const updateUI = (currentAccount) => {
   const { movements } = currentAccount
   const values = movements.map(({ value }) => value)
@@ -149,14 +195,86 @@ const showMovements = (currentAccount) => {
     containerMovements.insertAdjacentHTML('afterbegin', movHTML)
   })
 }
+
+// Implementación de la stranferencias
+
+// Obtención de la fecha con formato adecuado al modelo
+
+const transferDate = () => {
+  const fecha = new Date()
+  return formatDate(fecha)
+}
+
 btnTransfer.addEventListener('click', (e) => {
   e.preventDefault()
   const sendingAccount = accounts.find(
-    (account) => account.owner === inputTransferTo
+    (account) => account.owner === inputTransferTo.value
   )
+  const amount = Number(inputTransferAmount.value)
   if (
     sendingAccount &&
-    inputTransferAmount <= Number(labelBalance.textContent)
+    amount <= Number(labelBalance.textContent) &&
+    amount > 0
   ) {
+    const currentDate = transferDate()
+    logedAccount.movements.push({
+      date: currentDate,
+      value: -amount,
+    })
+    sendingAccount.movements.push({
+      date: currentDate,
+      value: amount,
+    })
+    updateUI(logedAccount)
   }
+  inputTransferTo.value = ''
+  inputTransferAmount.value = ''
+})
+
+//Implementación de solicitud de préstamos
+
+btnLoan.addEventListener('click', (e) => {
+  e.preventDefault()
+  console.log('estoy aqui')
+  const amount = Number(inputLoanAmount.value)
+  const currentDate = transferDate()
+  if (amount > 0)
+    logedAccount.movements.push({
+      date: currentDate,
+      value: amount,
+    })
+  updateUI(logedAccount)
+  inputLoanAmount.value = ''
+})
+
+//Implementacion de cerrar cuenta
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault()
+
+  if (
+    inputCloseUsername.value === logedAccount.username &&
+    Number(inputClosePin.value) === logedAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === logedAccount.username
+    )
+    console.log(index)
+    // .indexOf(23)
+    // Delete account
+    accounts.splice(index, 1)
+    // Hide UI
+    containerApp.style.opacity = 0
+  }
+  inputCloseUsername.value = inputClosePin.value = ''
+})
+
+// Implementación de la función sort
+
+btnSort.addEventListener('click', (e) => {
+  e.preventDefault()
+  logedAccount.movements = logedAccount.movements.sort(
+    (a, b) => a.value - b.value
+  )
+  updateUI(logedAccount)
 })
